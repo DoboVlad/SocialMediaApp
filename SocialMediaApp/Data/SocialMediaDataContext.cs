1c42cbd8bbd550aae8f66d4cee5bfebb5638d93c
DoboVlad <66554619+DoboVlad@users.noreply.github.com>
2020-10-17 18:49:17 +0300

    First Commit - with database connection, User Model and Password field in database

---
+++ SocialMediaApp/Data/SocialMediaDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaApp.Data
+{
+    public class SocialMediaDataContext: DbContext
+    {
+        public SocialMediaDataContext(DbContextOptions<SocialMediaDataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
--- SocialMediaApp/Startup.cs
+++ SocialMediaApp/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SocialMediaApp.Data;
 
 namespace SocialMediaApp
 {
@@ -25,6 +27,7 @@ namespace SocialMediaApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SocialMediaDataContext>(options => options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
